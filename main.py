9ed0d1dac1e2d75f54a059932d5323a9d034c7c8
simenen123 <73933280+simenen123@users.noreply.github.com>
2025-10-21 11:28:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import chapters
 import pdf_creator
 import web_scraper
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ""
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 api_rolle = "You are a Translator. One of your less experienced co-workers comes over with his work and you have to fix the erros they oversaw. Sometimes the co-worker zones out so text that look like ads get added and you need to remove those. Dont add any unessicary comments. Just give out the improved chapter."
